30be9a2be6e732bb1488950cfd22d1a3ec941391
Madhavgaba18 <madhavgaba42@gmail.com>
2025-07-14 14:56:55 +0530

    vector file added

--- vector.cpp
+++ vector.cpp
@@ -5,8 +5,24 @@ using namespace std;
 
 int main()
 {
-    vector<int> vec(3, 2);
-    cout << vec[1];
+    vector<int> vector_a;             // empty vector
+    vector<int> vector_b = {1, 2, 3}; // vector with initial values
+
+    vector<int> vector_c(3, 2); // vector with 3 elements, each initialized to 2
+    // cout << vector_c[0];
+    // cout << vector_c[1];
+    // cout << vector_c[2];
+
+    for (int val : vector_b)
+    {
+        cout << val << endl;
+    }
+
+    vector_a.push_back(1);
+    cout << vector_a[0] << endl;
+
+    int size = vector_c.size();
+    cout << size << endl;
 
     return 0;
 }
