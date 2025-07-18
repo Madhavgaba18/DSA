2b3c66d5a111545ab2cbac93aef0796859d0651b
Madhavgaba18 <madhavgaba42@gmail.com>
2025-07-19 01:25:34 +0530

    linear search and reverse on vector

--- vector.cpp
+++ vector.cpp
@@ -3,15 +3,50 @@
 
 using namespace std;
 
+// linear search
+int linear_search(vector<int> vec, int target)
+{
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (vec[i] == target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// reverse vector
+void reverse_vector(vector<int> vec)
+{
+    int start = 0, end = vec.size() - 1;
+    while (start < end)
+    {
+        swap(vec[start], vec[end]);
+        start++;
+        end--;
+    }
+    for (int val : vec)
+    {
+        cout << val << " ";
+    }
+}
+
 int main()
 {
-    vector<int> vector_a;             // empty vector
-    vector<int> vector_b = {1, 2, 3}; // vector with initial values
+    vector<int> vector_a;                      // empty vector
+    vector<int> vector_b = {1, 2, 3, 5, 6, 7}; // vector with initial values
 
     vector<int> vector_c(3, 2); // vector with 3 elements, each initialized to 2
-    // cout << vector_c[0];
-    // cout << vector_c[1];
-    // cout << vector_c[2];
+    cout << vector_c[0];
+    cout << vector_c[1];
+    cout << vector_c[2];
+
+    int res = linear_search(vector_b, 6);
+    cout << res;
+
+    reverse_vector(vector_b);
+    cout << endl;
 
     for (int val : vector_b)
     {
