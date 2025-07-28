60f36fbab4f1628df79b7973cd6e09a222dd86b5
Madhavgaba18 <madhavgaba42@gmail.com>
2025-07-28 23:44:51 +0530

    MissingNumbers - optimized

--- leetcode/MissingNumber - 268.cpp
+++ leetcode/MissingNumber - 268.cpp
@@ -22,3 +22,20 @@ public:
         return -1;
     }
 };
+
+// Optimized Approach using Sum Formula
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int n = nums.size();
+        int totalSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            actualSum += nums[i];
+        }
+        return totalSum - actualSum;
+    }
+};
