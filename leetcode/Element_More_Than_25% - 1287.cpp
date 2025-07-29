c0d631ceba83c233b14b80675f2361df933ef784
Madhavgaba18 <madhavgaba42@gmail.com>
2025-07-29 23:52:45 +0530

    Optimized 1287- Question

--- leetcode/Element_More_Than_25% - 1287.cpp
+++ leetcode/Element_More_Than_25% - 1287.cpp
@@ -22,3 +22,34 @@ public:
         return -1;
     }
 };
+
+// optimized approach for already sorted array
+class Solution
+{
+public:
+    int findSpecialInteger(vector<int> &arr)
+    {
+        int freq = 1;
+        int ans = arr[0];
+        if (arr.size() == 1)
+            return arr[0];
+        for (int i = 1; i < arr.size(); i++)
+        {
+            if (arr[i] == arr[i - 1])
+            {
+                freq++;
+            }
+            else
+            {
+                freq = 1;
+                ans = arr[i];
+            }
+
+            if (freq > arr.size() / 4)
+            {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+};
