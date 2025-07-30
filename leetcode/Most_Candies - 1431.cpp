1506b2113fa3df2e7bd30cf50e72a0b9789891e5
Madhavgaba18 <madhavgaba42@gmail.com>
2025-07-30 23:44:10 +0530

    Most Candies

---
+++ leetcode/Most_Candies - 1431.cpp
@@ -0,0 +1,28 @@
+// Brute Force Approach
+class Solution
+{
+public:
+    vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
+    {
+        vector<bool> result;
+        int greatest = 0;
+        for (int i = 0; i < candies.size(); i++)
+        {
+            greatest = max(greatest, candies[i]);
+        }
+
+        for (int i = 0; i < candies.size(); i++)
+        {
+            if (candies[i] + extraCandies < greatest)
+            {
+                result.push_back(false);
+            }
+            else
+            {
+                result.push_back(true);
+            }
+        }
+
+        return result;
+    }
+};
