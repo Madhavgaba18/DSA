85a9b884c6ad43005ce3944cc1a33a4f3ab8cc50
Madhavgaba18 <madhavgaba42@gmail.com>
2025-08-05 17:47:33 +0530

    conatins Duplicates

---
+++ leetcode/217 - ContainsDuplicate.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    bool containsDuplicate(vector<int> &nums)
+    {
+        bool ans = false;
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] == nums[i - 1])
+            {
+                return ans = true;
+            }
+        }
+        return ans;
+    }
+};
