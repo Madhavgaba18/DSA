45431cf2bafd21602d9127be4bfa8c07fd0a2b73
Madhavgaba18 <madhavgaba42@gmail.com>
2025-09-03 14:21:25 +0530

    SortColor - 75

--- leetcode/75 - SortColor.cpp
+++ leetcode/75 - SortColor.cpp
@@ -7,3 +7,43 @@ public:
         sort(nums.begin(), nums.end());
     }
 };
+
+// a better optimal approach but not fully optimized
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int count0 = 0, count1 = 0, count2 = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 0)
+            {
+                count0++;
+            }
+            else if (nums[i] == 1)
+            {
+                count1++;
+            }
+            else
+            {
+                count2++;
+            }
+        }
+
+        int index = 0;
+        for (int i = 0; i < count0; i++)
+        {
+            nums[index++] = 0;
+        }
+        for (int i = 0; i < count1; i++)
+        {
+            nums[index++] = 1;
+        }
+        for (int i = 0; i < count2; i++)
+        {
+            nums[index++] = 2;
+        }
+    }
+};
